5c29cae56c7aa227b9360b4a85de50d3cc9806ea
Fahad Akash <32955702+FahadAkash@users.noreply.github.com>
2023-12-28 19:46:49 +0600

    character movement with W A S D

---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Common;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody body;
+    public float MoveSpeed;
+    public Vector3 DirectionZ;
+    public Vector3 DirectionX;
+
+
+    private void FixedUpdate()
+    {
+        MovementSystem();
+    }
+
+    public void MovementSystem()
+    {
+        if(body != null)
+        {
+            if (Input.GetKey(KeyCode.W))
+            {
+                body.velocity += DirectionZ * MoveSpeed * Time.deltaTime;
+
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                body.velocity  -= DirectionZ * MoveSpeed * Time.deltaTime;   
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                body.velocity += DirectionX * MoveSpeed * Time.deltaTime;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                body.velocity -= DirectionX * MoveSpeed * Time.deltaTime;
+            }
+
+        }
+    }
+
+
+
+}
